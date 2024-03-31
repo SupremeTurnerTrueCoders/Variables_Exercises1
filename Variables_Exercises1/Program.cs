54399cb69d32edf61202989ede9df6facfc9fe1f
SupremeTurnerTrueCoders <email@example.com>
2024-03-30 21:18:16 -0400

    Add project files.

---
+++ Variables_Exercises1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Declare and initialize Variables 
+string name = "John";
+int age = 31;
+char middleInitial = "D";
+bool isover18 = true;
+double currentTemp =77.9;
+decimal currentPrice =29.99m;
+
+//Writing variables to console 
+Console.WriteLine($"name: {name})";
+Console.WriteLine($"age: {age})";
+Console.WriteLine($":middleInitial: {middleInitial})";
+Console.WriteLine($"Bool: {bool})";
+Console.WriteLine($"Double: {Double})";
+Console.WriteLine($"Decimal: {Decimal})";
+
+                  
+                  
+
+
+
+
+   
